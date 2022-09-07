ac696b99171a540bf8e35110004ac144bab28844
Yongchin <yongchin39@qq.com>
2022-09-07 16:05:33 +0800

    chore: UMD build setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  //umd build
+  optimizeDeps: { include: ["cronstrue"] },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/cronstrue/, /node_modules/],
+    },
+  },
+});
