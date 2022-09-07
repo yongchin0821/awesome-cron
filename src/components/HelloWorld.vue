e9bb4012b393096e965e3e39c6a417ce4a317575
Yongchin <yongchin39@qq.com>
2022-09-07 10:53:38 +0800

    feat: parser6 & parser7 done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ const datas = reactive({
   end: '."',
   full: "",
   textCount: 5,
-  toHighlight: null,
+  cursor: null,
   datetime: null,
 });
 
@@ -19,6 +19,7 @@ type TparsedText = {
   mond?: string;
   mon?: string;
   wkd?: string;
+  year?: string;
   AP?: string;
 };
 
@@ -30,6 +31,7 @@ const parsedText = ref<TparsedText>({
   mond: "",
   mon: "",
   wkd: "",
+  year: "",
   AP: "",
 });
 
@@ -41,7 +43,6 @@ const desc = ref<string>(
 
 const formatter = (text: string) => {
   let l = text.split(" ").filter((item) => item != "");
-  datas.textCount = l.length;
   return l.join(" ");
 };
 
@@ -55,31 +56,92 @@ const parser5 = () => {
   let startindex = ele.selectionStart;
   let text = crontext.value.substring(0, startindex);
   let textList = formatter(text).split(" ");
-  console.log(textList);
   switch (textList.length) {
     case 1:
-      datas.toHighlight = "min";
+      datas.cursor = "min";
       break;
     case 2:
-      datas.toHighlight = "hour";
+      datas.cursor = "hour";
       break;
     case 3:
-      datas.toHighlight = "mond";
+      datas.cursor = "mond";
       break;
     case 4:
-      datas.toHighlight = "mon";
+      datas.cursor = "mon";
       break;
     case 5:
-      datas.toHighlight = "wkd";
+      datas.cursor = "wkd";
       break;
   }
   highlight();
+  changDdesc();
+};
+
+const parser6 = () => {
+  let ele: HTMLElement = document.getElementById("crontext");
+  let startindex = ele.selectionStart;
+  let text = crontext.value.substring(0, startindex);
+  let textList = formatter(text).split(" ");
+  switch (textList.length) {
+    case 1:
+      datas.cursor = "sec";
+      break;
+    case 2:
+      datas.cursor = "min";
+      break;
+    case 3:
+      datas.cursor = "hour";
+      break;
+    case 4:
+      datas.cursor = "mond";
+      break;
+    case 5:
+      datas.cursor = "mon";
+      break;
+    case 6:
+      datas.cursor = "wkd";
+      break;
+  }
+  highlight();
+  changDdesc();
+};
+
+const parser7 = () => {
+  let ele: HTMLElement = document.getElementById("crontext");
+  let startindex = ele.selectionStart;
+  let text = crontext.value.substring(0, startindex);
+  let textList = formatter(text).split(" ");
+  switch (textList.length) {
+    case 1:
+      datas.cursor = "sec";
+      break;
+    case 2:
+      datas.cursor = "min";
+      break;
+    case 3:
+      datas.cursor = "hour";
+      break;
+    case 4:
+      datas.cursor = "mond";
+      break;
+    case 5:
+      datas.cursor = "mon";
+      break;
+    case 6:
+      datas.cursor = "wkd";
+      break;
+    case 7:
+      datas.cursor = "year";
+      break;
+  }
+  highlight();
+  changDdesc();
 };
 
 const highlight = () => {
-  for (var item of ["min", "hour", "mond", "mon", "wkd"]) {
+  for (var item of ["sec", "min", "hour", "mond", "mon", "wkd", "year"]) {
     let eles = Array.from(document.getElementsByName(item));
-    if (item == datas.toHighlight) {
+    if (item == datas.cursor) {
       eles.map((e) => {
         e.style.color = "gray";
       });
@@ -91,21 +153,63 @@ const highlight = () => {
   }
 };
 
+const changDdesc = () => {
+  switch (datas.cursor) {
+    case "sec":
+      desc.value =
+        "*	any value\n,	value list separator\n-	range of values\n/	step values";
+      break;
+    case "min":
+      desc.value =
+        "*	any value\n,	value list separator\n-	range of values\n/	step values\n0-23	allowed values";
+      break;
+    case "hour":
+      desc.value =
+        "*	any value\n,	value list separator\n-	range of values\n/	step values\n0-59	allowed values";
+      break;
+    case "mond":
+      desc.value =
+        "*	any value\n,	value list separator\n-	range of values\n/	step values\n1-31	allowed values";
+      break;
+    case "mon":
+      desc.value =
+        "*	any value\n,	value list separator\n-	range of values\n/	step values\n1-12	allowed values\nJAN-DEC	alternative single values";
+      break;
+    case "wkd":
+      desc.value =
+        "*	any value\n,	value list separator\n-	range of values\n/	step values\n0-6	allowed values\nSUN-SAT	alternative single values\n7	sunday (non-standard)";
+      break;
+    case "year  ":
+      desc.value =
+        "*	any value\n,	value list separator\n-	range of values\n/	step values\n0-23	allowed values";
+      break;
+  }
+  ["sec", "min", "hour", "mond", "mon", "wkd", "year"].map((i) => {
+    if (i != datas.cursor) {
+      document.getElementById("span-" + i).style.textDecoration = "none";
+    } else {
+      document.getElementById("span-" + i).style.textDecoration = "underline";
+    }
+  });
+};
+
 const strategies = {
   5: parser5,
-  6: () => {},
-  7: () => {},
+  6: parser6,
+  7: parser7,
 };
 
 const cursorParsing = (e) => {
   // format textCount
-  formatter(e.target.value);
+  let fstr = formatter(e.target.value);
+  datas.textCount = fstr.split(" ").length;
   strategies[datas.textCount]();
 };
 
 // crontextParsing
 const getParsedText = () => {
   parsedText.value = cronstrue.toDetails(crontext.value);
+  console.log(parsedText.value);
   if (parsedText.value.full.substring(0, 7).indexOf(":") == -1) {
     datas.datetime = false;
     parsedText.value.min = parsedText.value.min.replace("at ", "");
@@ -113,8 +217,8 @@ const getParsedText = () => {
   } else {
     datas.datetime = true;
     parsedText.value.hour = parsedText.value.full.substring(2, 5);
-    parsedText.value.min = parsedText.value.full.substring(6, 9);
-    parsedText.value.AP = parsedText.value.full.slice(-2);
+    parsedText.value.min = parsedText.value.full.substring(6, 8);
+    parsedText.value.AP = parsedText.value.full.split(",")[0].slice(-3);
   }
 };
 
@@ -122,7 +226,7 @@ watch(crontext, (newValue, oldValue) => {
   formatter(newValue);
   try {
     getParsedText();
-    console.log(parsedText.value);
+    // console.log(parsedText.value);
   } catch (e) {
     console.log(e);
   }
@@ -132,8 +236,8 @@ const ptext = ref();
 
 onMounted(() => {
   getParsedText();
-  console.log(datas.datetime);
-  console.log(ptext);
+  // console.log(datas.datetime);
+  // console.log(ptext);
 });
 </script>
 
@@ -145,11 +249,13 @@ onMounted(() => {
       <span name="hour">{{ parsedText.hour }}</span>
       <span>:</span>
       <span name="min">{{ parsedText.min }}</span>
+      <span v-if="parsedText.sec != null">:</span>
       <span name="sec">{{ parsedText.sec }}</span>
       <span name="hour">{{ parsedText.AP }}</span>
       <span name="mond">{{ parsedText.mond }}</span>
       <span name="mon">{{ parsedText.mon }}</span>
       <span name="wkd">{{ parsedText.wkd }}</span>
+      <span name="year">{{ parsedText.year }}</span>
       {{ datas.end }}
     </h1>
     <h1 v-else>
@@ -161,6 +267,7 @@ onMounted(() => {
       <span name="mond">{{ parsedText.mond }}</span>
       <span name="mon">{{ parsedText.mon }}</span>
       <span name="wkd">{{ parsedText.wkd }}</span>
+      <span name="year">{{ parsedText.year }}</span>
       {{ datas.end }}
     </h1>
   </div>
@@ -175,16 +282,18 @@ onMounted(() => {
     />
   </div>
   <div class="desc">
-    <div>
-      <span>minute</span>
-    </div>
-    <div>
-      <span>hour</span>
-    </div>
-    <div>day</div>
-    <div>week</div>
-    <div>month</div>
-    <textarea>{{ desc }}</textarea>
+    <p>
+      <span id="span-sec" v-show="datas.textCount == 6 || datas.textCount == 7">
+        second
+      </span>
+      <span id="span-min"> minute </span>
+      <span id="span-hour"> hour</span>
+      <span id="span-mond"> day </span>
+      <span id="span-mon"> month </span>
+      <span id="span-wkd"> week </span>
+      <span id="span-year" v-show="datas.textCount == 7"> year </span>
+    </p>
+    <textarea cols="40" rows="10">{{ desc }}</textarea>
   </div>
 
   <p>
@@ -210,5 +319,10 @@ onMounted(() => {
   width: 80%;
   font-size: 30px;
   text-align: center;
+}
+.desc textarea {
+  font-size: large;
+  resize: none;
+  border: none;
 }
 </style>
