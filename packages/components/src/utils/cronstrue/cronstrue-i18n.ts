79674e2533f078bf6b367930b3bb119cd345ed0b
Yongchin <yongchin39@qq.com>
2022-09-29 17:08:12 +0800

    feat: update i18n

--- packages/components/src/utils/cronstrue/cronstrue-i18n.ts
+++ packages/components/src/utils/cronstrue/cronstrue-i18n.ts
@@ -5,4 +5,5 @@ ExpressionDescriptor.initialize(new allLocalesLoader());
 export default ExpressionDescriptor;
 
 let toString = ExpressionDescriptor.toString;
-export { toString };
+let toDetails = ExpressionDescriptor.toDetails;
+export { toString, toDetails };
